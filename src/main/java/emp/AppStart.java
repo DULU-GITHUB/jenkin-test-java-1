483cb2ad0a57a5bf38e65a2a06d046ce599d7cff
unknown <g6.manoranjan@gmail.com>
2021-05-31 13:24:24 +0530

    files updated

--- src/main/java/emp/AppStart.java
+++ src/main/java/emp/AppStart.java
@@ -9,7 +9,7 @@ public class AppStart {
 	public static void main(String[] args) {
 		//SpringApplication.run(AppStart.class, args);
 		System.out.println("This is java program");
-		System.out.println("This is AppStart class");
+		System.out.println("This is AppStart class for test 1");
 	}
 
 }
